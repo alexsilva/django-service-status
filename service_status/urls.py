405a646e0f3a3180735eba2d8741efbf9ef6551e
alexsilva <geniofuturo@gmail.com>
2024-04-29 09:36:08 -0300

    Adaptations for django4.

--- service_status/urls.py
+++ service_status/urls.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, print_function, unicode_literals
-
-from django.conf.urls import url
-
+# coding=utf-8
+try:
+    from django.urls import re_path as url
+except ImportError:
+    from django.conf.urls import url
 from service_status.views import ServiceStatusView
 
 # name this app
